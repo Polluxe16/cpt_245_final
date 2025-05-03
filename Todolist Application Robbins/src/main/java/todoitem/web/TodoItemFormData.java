d4376f5c6941d6ba96bfbd0bfd9b9165f89aa73f
Polluxe <Polluxe16@outlook.com>
2025-05-02 20:51:36 -0400

    Create TodoItemFormData.java

---
+++ Todolist Application Robbins/src/main/java/todoitem/web/TodoItemFormData.java
@@ -0,0 +1,21 @@
+import javax.validation.constraints.NotBlank;
+
+public class TodoItemFormData {
+    @NotBlank
+    private String title;
+}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+        
+    public enum ListFilter {
+    ALL,
+    ACTIVE,
+    COMPLETED
+        }
+    
+}
