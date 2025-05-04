b2f7a403ff5b6e2e5dc87e799b0f244d3cc3a87d
Polluxe <Polluxe16@outlook.com>
2025-05-04 17:40:42 -0400

    Update TodoItemFormData.java

--- Todolist Application Robbins/src/main/java/com/AidJRobbins/examples/todomvcthymeleaf/todoitem/web/TodoItemFormData.java
+++ Todolist Application Robbins/src/main/java/com/AidJRobbins/examples/todomvcthymeleaf/todoitem/web/TodoItemFormData.java
@@ -1,3 +1,5 @@
+//This is the Form Data for the project, which corresponds to the <form> section in index.html
+
 package com.wimdeblauwe.examples.todomvcthymeleaf.todoitem.web;
 
 import javax.validation.constraints.NotBlank;
